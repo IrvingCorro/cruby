4c0cc2728466bcc104eb184df31bae4c78321fad
irving <irvingsk19@hotmail.com>
2014-04-18 08:13:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cruby::Application.config.secret_key_base = '6c43b7cc38c0e13be5492b7c2e9e6e6f7db1011caddf652b37cc574b4a61e68354dd6397fa05abfa60e58ead0dbe4b8c366a4c1655625894908eb20c1d5e5df1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cruby::Application.config.session_store :cookie_store, key: '_cruby_session'
